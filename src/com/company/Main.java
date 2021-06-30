70993b9baf5e9b335c06dca5113fd9d4e9770c55
tainaoual <tainaoual@gmail.com>
2021-06-29 21:27:49 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code hereCreate a class called MyClass. Make four fields, make one private,
+        //    one public, one protected, and one default. Name the fields one, two,
+        //    three, four.
+
+
+
+
+    }
+}
---
+++ src/com/company/Tree.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Tree {
+
+    private  String name3;
+   ;
+
+
+}
